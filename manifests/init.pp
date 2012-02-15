9b5c07afcf4e0b18f48346093053a7d46222a19a
Kris Buytaert <sdog@inuits.be>
2012-02-15 23:17:56 +0100

    Puppet Linit is now happy

--- manifests/init.pp
+++ manifests/init.pp
@@ -13,39 +13,43 @@
 # [Remember: No empty lines between comments and class definition]
 class jmxtrans {
 
-  package {"jmxtrans": 
-    ensure => present;} 
+  package {'jmxtrans':
+    ensure => present;
+  }
 
 
-   # Current JMX trans service script doesn't return exict codes correctly,  patch subbmitted upstream and accepted. 
-   # Next JMX trans will  have good rc script 
+  # Current JMX trans service script doesn't return exict codes correctly,
+  # patch subbmitted  upstream and accepted.
+  # Next JMX trans will  have good rc script
 
-  service {"jmxtrans": 
+  service {'jmxtrans':
     ensure    => running,
     hasstatus => true,
     enable    => true ; }
 }
- class jmxtrans::example {
+class jmxtrans::example {
 
   file { '/var/lib/jmxtrans/localhost.json':
     group   => '0',
-    mode    => '644',
+    mode    => '0644',
     owner   => '0',
-    source  => "puppet:///modules/jmxtrans/localhost.json";
+    source  => 'puppet:///modules/jmxtrans/localhost.json';
   }
 
 }
 
 
-# Currently mostly using it to send stuff to graphite 
-# Need to create a ganglia template too 
+# Currently mostly using it to send stuff to graphite
+# Need to create a ganglia template too
 
-define jmxtrans::graphite ( $jmxport, $jmxhost, $objtype, $attributes, $graphiteport, $graphitehost ,$typenames='',$resultAlias='')
+define jmxtrans::graphite ( $jmxport, $jmxhost, $objtype, $attributes,
+                            $graphiteport, $graphitehost ,$typenames='',
+                            $resultAlias='')
 {
   file { "/var/lib/jmxtrans/${name}.json":
-    mode     => '644',
+    mode     => '0644',
     owner    => '0',
     group    => '0',
-    content  => template("jmxtrans/json.graphite.erb");
+    content  => template('jmxtrans/json.graphite.erb');
   }
 }
